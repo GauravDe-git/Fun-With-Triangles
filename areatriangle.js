1274a61280606379b9f0b6405cd40e021a08762a
Gaurav De <gm.gauravde@gmail.com>
2021-09-22 00:57:53 +0530

    fix: negative number bug

--- areatriangle.js
+++ areatriangle.js
@@ -4,8 +4,13 @@ const outputText = document.querySelector("#output-area");
 
 function calculateArea()
 {
-    const area = 1/2 * sidesInput[0].value * sidesInput[1].value;
-    outputText.innerText = "The Area of this Triangle is: " + area;
+    if(sidesInput[0].value < 0 || sidesInput[1].value < 0)
+    {
+        outputText.innerText = "Sides can't be negative";
+    }
+    else
+    {const area = 1/2 * sidesInput[0].value * sidesInput[1].value;
+    outputText.innerText = "The Area of this Triangle is: " + area.toFixed(2);}
 }
 
 calculateBtn.addEventListener("click",calculateArea);
